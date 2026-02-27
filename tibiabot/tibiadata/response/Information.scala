549d66bca3a6f4dee0c3044339a8dead298fd94e
Xanshara <bartek.rychel@outlook.com>
2026-02-27 15:50:07 +0100

    v2.3e-fix

--- tibiabot/tibiadata/response/Information.scala
+++
@@ -1,7 +0,0 @@
-package com.tibiabot.tibiadata.response
-
-case class Api(version: Double, release: String, commit: String)
-
-case class Status(http_code: Double)
-
-case class Information(api: Api, status: Status)
